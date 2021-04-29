6e0171ed5515cb42baac34f9afc6cfb8cf04c222
muddukrishnahm <39989482+muddukrishnahm@users.noreply.github.com>
2021-04-29 15:05:22 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+yum install wget 
+wget https://get.helm.sh/helm-v3.5.4-linux-amd64.tar.gz
+tar -xvf helm-v3.5.4-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin/helm
